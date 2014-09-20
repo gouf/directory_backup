cf4ab341106935182f8175bf53fa4858380d53cc
gouf <innocent.zero@gmail.com>
2014-09-20 23:05:40 +0900

    Add upload methods

--- s3_backup.rb
+++ s3_backup.rb
@@ -42,4 +42,21 @@ class S3Backup
     @bucket_name       = c['bucket_name']
     @directory_prefix  = c['directory_prefix']
   end
+
+  def upload
+    @targets.each do |file_path|
+      puts file_path
+      bucket.objects[file_path].write(file_path)
+    end
+  end
+
+  def bucket
+    @s3.buckets[@bucket_name]
+  end
+
+  def buckets
+    @s3.buckets
+  end
+
+  alias_method :buckup, :upload
 end
