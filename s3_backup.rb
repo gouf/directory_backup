683577ea534c088a1f26bb2132ce77d264fe6130
gouf <innocent.zero@gmail.com>
2014-09-20 19:12:10 +0900

    Add AWS S3 setup code

--- s3_backup.rb
+++ s3_backup.rb
@@ -14,6 +14,10 @@ class S3Backup
       add_target(path)
     end
     @targets.uniq!
+    @s3 = AWS::S3.new(
+      access_key_id: @access_key_id,
+      secret_access_key: @secret_access_key
+    )
   end
 
   def load_config
